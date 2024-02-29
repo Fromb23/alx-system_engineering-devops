9419856c61d2332d22a3529a2c17cae2577ae381
Fromb23 <rombo.programming@gmail.com>
2024-02-29 12:12:25 -0800

    ssearch regex simplified

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-if ARGV.length != 1
-  puts "wrong number of command line args"
-  exit
-end
-
-#Get the args
-args = ARGV[0]
-
-#Find the regex match
-matches = args.scan(/hb*t*n/)
-
-#Print the match
-puts matches.join
+puts ARGV[0].scan(/hb*t*n/).join if ARGV.length == 1 || exit
