a4f6ac85b79b95bc941a263cdbabaef379525c82
Fromb23 <rombo.programming@gmail.com>
2024-02-29 12:10:05 -0800

    matching the occurrence of b

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ruby
 if ARGV.length != 1
   puts "wrong number of command line args"
   exit
@@ -8,7 +8,7 @@ end
 args = ARGV[0]
 
 #Find the regex match
-matches = args.scan(/hb+tn/)
+matches = args.scan(/hb*t*n/)
 
 #Print the match
 puts matches.join
