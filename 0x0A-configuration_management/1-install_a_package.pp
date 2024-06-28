9f1daf68d850c072f37c11e1907ef30b1bbf3900
Fromb23 <rombo.programming@gmail.com>
2024-06-28 17:04:26 +0300

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,5 @@
-# Install a package using puppet
-class { 'python':
-  version => '3',
-}
-
-python::pyvenv { '/opt/myenv':
-  ensure   => 'present',
-  version  => '3',
-  pip      => 'present',
-  systempkgs => 'false',
-  owner    => 'root',
-  group    => 'root',
-}
-
-python::pip { 'flask':
-  ensure  => '2.1.0',
-  virtualenv => '/opt/myenv',
-  pip_provider => 'pip3',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
