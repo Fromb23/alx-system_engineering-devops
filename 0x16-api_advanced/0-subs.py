0f2936f71b4d163dfc75173cbb73fa18932e9da7
Fromb23 <rombo.programming@gmail.com>
2024-08-13 21:45:12 +0300

    subtitile

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,10 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirect=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
         return 0
